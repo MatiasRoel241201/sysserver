ab355a9798666009ee0f446f6170303d18f07b15
Matias Roel <matiasroel241201@gmail.com>
2025-11-30 23:25:11 -0300

    Configuracion de CORS

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,11 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ 
-    origin: 'http://localhost:3000',
+  app.enableCors({
+    origin: [
+      'http://localhost:3000',
+      'https://sysfront-production.up.railway.app',
+    ],
     credentials: true,
   });
   app.setGlobalPrefix('api');
